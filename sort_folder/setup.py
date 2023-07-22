6e7843fc2cb05c3f93d68da47e62fba410bcbe98
Oleksandr <alexsentex7@gmail.com>
2023-07-23 01:23:01 +0300

    Update setup.py

--- sort_folder/setup.py
+++ sort_folder/setup.py
@@ -8,7 +8,7 @@ setup(
     description='Sort files',
     long_description=description,
     author='Semochkin Oleksandr',
-    url='https://github.com/AlexSentex/go-it-hw-6/blob/914ca550341dbed843e03529e26406bdc8fa7899/Homework.py',
+    url='https://github.com/AlexSentex/go-it-hw-7.git',
     requires=find_namespace_packages(),
     license='MIT',
     entry_points={'console_scripts': ["sort_folder = sort_folder.Homework:main"]}
